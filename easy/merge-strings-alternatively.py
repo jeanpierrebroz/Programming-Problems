c4160e67c79480e14280718439cb9cf341c04f26
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-01-21 12:44:28 -0700

    can-place-flowers

--- easy/merge-strings-alternatively.py
+++ easy/merge-strings-alternatively.py
@@ -1,29 +1,5 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
-        ''' 
-        naive approach: O(m+n) space, O(m*n) time complexity b/c of string 
-        immutability
-
-        l1, l2 = len(word1), len(word2)
-        result = ""
-
-        for i in range(min(l1, l2)):
-            result += word1[i]
-            result += word2[i]
-
-        if l1 > l2:
-            result += word1[i+1:]
-            return result
-        
-        elif l2 > l1:
-            result += word2[i+1:]
-
-        return result
-
-        '''
-
-        #optimized: use list for O(n) time instead
         l1, l2 = len(word1), len(word2)
         result = []
 
