cdcfd01c88c8961806fe7de6e7e904d216bc44a2
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-02-04 20:18:39 -0700

    kth-largest-element-in-array

--- easy/maximum-depth-of-binary-tree.py
+++ easy/maximum-depth-of-binary-tree.py
@@ -3,4 +3,4 @@ class Solution:
         if not root:
             return 0
 
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))     
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))      
