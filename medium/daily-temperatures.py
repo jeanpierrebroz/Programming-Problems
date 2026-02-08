10f78ec3c8a5143c03d40c6f5b8abd4a1105c39e
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-02-08 16:26:24 -0700

    daily-temperatures

---
+++ medium/daily-temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0 for _ in temperatures]
+        stack = []
+
+        for i, t in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < t:
+                answer[stack[-1]] = i - stack[-1]
+                stack.pop()
+
+            stack.append(i)
+        
+        return answer
