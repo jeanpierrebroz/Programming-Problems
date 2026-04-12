8b370147bb69266950eb3afdf66d1fcbc69dff2f
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-04-12 11:18:08 -0600

    recursive

--- dsa-implementations/dfs.py
+++ dsa-implementations/dfs.py
@@ -9,8 +9,20 @@ class GraphNode:
     def add_neighbors(self, neighbors):
         self.neighbors.extend(neighbors)
 
-def dfs_recursive(graph):
-    pass
+def dfs_recursive(node):
+    seen = set()
+    result = []
+    
+    def helper(node):
+        seen.add(node)
+        result.append(node.val)
+        for neighbor in node.neighbors:
+            if neighbor not in seen:
+                helper(neighbor)
+    
+    helper(node)
+    
+    return result
     
 def dfs_iterative(node: GraphNode):
     seen = set()
@@ -23,7 +35,6 @@ def dfs_iterative(node: GraphNode):
         curr_node = stack.pop()
         
         if curr_node not in seen:
-            
             seen.add(curr_node)
             stack.extend(reversed([n for n in curr_node.neighbors if n not in seen]))
             result.append(curr_node.val)
@@ -49,22 +60,21 @@ class DfsTests(unittest.TestCase):
         
         self.assertEqual([1, 2, 4, 5, 3], result)
         
-    # def test_recursive_dfs(self):
-    #     n1 = GraphNode(1)
-    #     n2 = GraphNode(2)
-    #     n3 = GraphNode(3)
-    #     n4 = GraphNode(4)
-    #     n5 = GraphNode(5)
+    def test_recursive_dfs(self):
+        n1 = GraphNode(1)
+        n2 = GraphNode(2)
+        n3 = GraphNode(3)
+        n4 = GraphNode(4)
+        n5 = GraphNode(5)
         
-    #     n1.add_neighbors([n2, n3])
-    #     n2.add_neighbors([n4, n5])
+        n1.add_neighbors([n2, n3])
+        n2.add_neighbors([n4, n5])
         
-    #     n3.add_neighbors([n1, n2])
+        n3.add_neighbors([n1, n2])
         
-    #     result = dfs_recursive(n1)
+        result = dfs_recursive(n1)
         
-    #     self.assertEqual([1, 3, 2, 5, 4], result)
-    
+        self.assertEqual([1, 2, 4, 5, 3], result)    
     
 if __name__=="__main__":
     unittest.main()
