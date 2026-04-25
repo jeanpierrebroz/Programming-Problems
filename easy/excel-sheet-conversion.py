39ddd6f3d25a4f8ab28983afd0835fe3e9a59c11
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-04-25 11:37:52 -0600

    Create excel-sheet-conversion.py

---
+++ easy/excel-sheet-conversion.py
@@ -0,0 +1,6 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        if columnNumber == 0:
+            return ""
+        
+        return self.convertToTitle((columnNumber - 1) // 26) + chr((columnNumber - 1) % 26 + ord('A'))
